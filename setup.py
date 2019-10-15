26ec024811a5cf1292e47c376ab8a8ecae27539e
J.P. Leibundguth <jleibund@gmail.com>
2019-10-15 00:37:17 -0700

    remove cython, no added benefit here

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
 from setuptools import setup, find_packages
-from distutils.core import setup
-from Cython.Build import cythonize
 
-setup(name='followme', version='1.0', packages=find_packages(), ext_modules=cythonize("pilots/*.py"))
+setup(name='followme', version='1.0', packages=find_packages())
